19ea761b753fd9bc624f551ddf5087d3f4bdfb61
kush mistry <kushmistry.divine116@gmail.com>
2025-05-21 23:17:12 +0530

    One json file to practice purpose

---
+++ JS/json.js
@@ -0,0 +1,10 @@
+// const jsonString = '{"name": "Alice", "age": 30}';
+// const obj = JSON.parse(jsonString);
+// console.log(obj.name); // Alice
+
+
+let myArr = ["John", "Mary", "Peter", "Sally"];
+
+let myJSON = JSON.parse(myArr);
+
+console.log(myJSON);    
